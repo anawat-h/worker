fc6d76002bf0a7d7b5ea45eb34b00de133c6ca34
anawat.h <anawat.h@clicknext.com>
2023-07-18 17:41:25 +0700

    shared worker

--- worker.js
+++ worker.js
@@ -1,5 +1,9 @@
-let counter = 0
-self.addEventListener('message', () => {
-  counter++
-  postMessage(counter)
-})
+self.onconnect = function (event) {
+  const port = event.ports[0]
+  let counter = 0
+  port.addEventListener('message', () => {
+    counter++
+    port.postMessage(counter)
+  })
+  port.start()
+}
